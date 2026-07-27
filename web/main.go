9da41739323d9e261c709a7b254bc7a3ee9ef65f
Christopher Fujino <christopherfujino@gmail.com>
2026-07-26 17:47:42 -0700

    move web/assets -> docs/ for gh pages

--- web/main.go
+++ web/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var handler = http.FileServer(http.Dir("./assets"))
+	var handler = http.FileServer(http.Dir("../docs"))
 	const addr = "0.0.0.0:8080"
 	fmt.Printf("Server listening at %s\n", addr)
 	http.ListenAndServe(addr, handler)
